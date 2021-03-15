07760075387c7974942b7eeef44c6baf96b80437
Rash <gizarash@gmail.com>
2021-03-15 13:36:38 +0600

    feat: implement min, max and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,55 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (Array.isArray(array)) {
+    if (array.length == 0) {
+      return 0;
+    } else {
+      let min = array[0];
+      for (let i = 1; i < array.length; i++) {
+        const element = array[i];
+        if (element < min) {
+          min = element;
+        }
+      }
+      return min;
+    }
+  } else {
+    return 0;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (Array.isArray(array)) {
+    if (array.length == 0) {
+      return 0;
+    } else {
+      let max = array[0];
+      for (let i = 1; i < array.length; i++) {
+        const element = array[i];
+        if (element > max) {
+          max = element;
+        }
+      }
+      return max;
+    }
+  } else {
+    return 0;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (Array.isArray(array)) {
+    if (array.length == 0) {
+      return 0;
+    } else {
+      let sum = 0;
+      for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        sum += element;
+      }
+      return (sum/array.length);
+    }
+  } else {
+    return 0;
+  }
 }
